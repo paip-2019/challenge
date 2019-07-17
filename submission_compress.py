9bbcf05d6dc5d59e4a7169459004d89999b376b8
hjoonjang <jjaang2570@naver.com>
2019-07-17 20:07:24 +0900

    add type casting

--- submission_compress.py
+++ submission_compress.py
@@ -1,5 +1,6 @@
 from tifffile import imsave
 import skimage.io as io
+import numpy as np
 
 # an example filename
 submission_filename = 'case_083.tif'
@@ -7,6 +8,7 @@ submission_filename = 'case_083.tif'
 
 # load image: any library you're familiar with
 img = io.imread(fn)
+img = img.astype(np.uint8) # NOTE: tifffile.imsave would show a 'bilevel'-related error w/o this explicit type casting
 
 # save with compression: ADOBE_DEFLATE algorithm with level 9 (among 0~9)
 # NOTE: the saving filename is the same with the loading filename. In other words, it may overwrite your original image.
