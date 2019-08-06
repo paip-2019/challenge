9c4d266f7861a4119c0ca727d89682313d261261
hjoonjang <jjaang2570@naver.com>
2019-08-06 18:35:55 +0900

    190806

--- submission_compress.py
+++ submission_compress.py
@@ -4,12 +4,12 @@ import numpy as np
 
 # an example filename
 submission_filename = 'case_083.tif'
-# print(fn)
+# print(submission_filename)
 
 # load image: any library you're familiar with
-img = io.imread(fn)
+img = io.imread(submission_filename)
 img = img.astype(np.uint8) # NOTE: tifffile.imsave would show a 'bilevel'-related error w/o this explicit type casting
 
 # save with compression: ADOBE_DEFLATE algorithm with level 9 (among 0~9)
 # NOTE: the saving filename is the same with the loading filename. In other words, it may overwrite your original image.
-imsave(fn, img, compress=9)
+imsave(submission_filename, img, compress=9)
